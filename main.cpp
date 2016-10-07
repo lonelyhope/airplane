2af68da309fd5e939af1270e62cc100fe47faf47
mukae1997 <444652692@qq.com>
2016-10-07 10:41:24 +0800

    Update main.cpp
    配置两条跑道
        take_off_runway take_off(queue_limit);  
        land_on_runway land_on(queue_limit);  
    修改run_idle函数
    
    void run_idle_takeoff(int time)
    void run_idle_landon(int time)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include "Extended_queue.cpp"
-#include "Runway.cpp"
+
+
+#include "Runway.h"
 using namespace std;
+
 
 
 void initialize(int &end_time, int &queue_limit,
@@ -34,13 +36,40 @@ if (acceptable && arrival_rate  + departure_rate > 1.0)
 cerr << "Safety Warning: This airport will become saturated. " << endl;
 } while (!acceptable);
 }
-void run_idle(int time){
+/*
+void test_poisson() {
+int n;cin>> n;
+int sum = 0;
+Random p(0);
+int temp;
+for (int i = 0; i < 10;i++) {
+    temp = p.poisson(n);
+    cout << temp<<endl;
+    sum+=temp;
+}
+
+cout <<"average:  "<<sum/1000<<endl;
+
+}
+*/
+void run_idle_takeoff(int time){
 /* Post: The specified time is printed with a message that the runway is idle. */
 
-cout << time << ": Runway is idle." << endl;
+cout << time << ": Take-off Runway is idle." << endl;
 }
- 
-int main( ) // Airport simulation program
+
+void run_idle_landon(int time){
+/* Post: The specified time is printed with a message that the runway is idle. */
+
+cout << time << ": Land-on Runway is idle." << endl;
+}
+
+int main( ) {
+/*
+    test_poisson();
+    system("pause");
+    // Airport simulation program
+*/
 /* Pre: The user must supply the number of time intervals the simulation is to run,
 the expected number of planes arriving, the expected number of planes
 departing per time interval, and the maximum allowed size for runway
@@ -49,44 +78,73 @@ Post: The program performs a random simulation of the airport, showing the
 status of the runway at each time interval, and prints out a summary of
 airport operation at the conclusion.
 Uses: Classes Runway, Plane, Random and functions run_idle, initialize. */
-{
-int end_time; // time to run simulation
-int queue_limit; // size of Runway queues
-int flight_number = 0;
-double arrival_rate, departure_rate;
-initialize(end_time, queue_limit, arrival_rate, departure_rate);
-Random variable;
-Runway small_airport(queue_limit);
-for (int current_time = 0; current_time < end_time; current_time++) {
-// loop over time intervals
-int number_arrivals = variable.poisson(arrival_rate);
-// current arrival requests
-for (int i = 0; i < number_arrivals; i++) {
-Plane current_plane(flight_number++, current_time, arriving);
-if (small_airport.can_land(current_plane) != success)
-current_plane.refuse( );
-}
-int number_departures = variable.poisson(departure_rate);
-// current departure requests
-for (int j = 0; j < number_departures; j++) {
-Plane current_plane(flight_number++, current_time, departing);
-if (small_airport.can_depart(current_plane) != success)
-current_plane.refuse( );
-}
-Plane moving_plane;
-switch (small_airport.activity(current_time, moving_plane)) {
-// Let at most one Plane onto the Runway at current_time.
-case land:
-moving_plane.land(current_time);
-break;
-case takeoff:
-moving_plane.fly(current_time);
-break;
-case idle:
-run_idle(current_time);
-}
-}
-small_airport.shut_down(end_time);
+
+    int end_time; // time to run simulation
+    int queue_limit; // size of Runway queues
+    int flight_number = 0;
+    double arrival_rate, departure_rate;
+
+    initialize(end_time, queue_limit, arrival_rate, departure_rate);
+    Random variable;
+
+    cout<< end_time<<endl; // time to run simulation
+    cout<< queue_limit<<endl; // size of Runway queues
+    cout<< arrival_rate<<endl;
+    cout<< departure_rate<<endl;
+
+
+//  MAKE runways.
+    take_off_runway take_off(queue_limit);  
+    land_on_runway land_on(queue_limit);  
+
+    for (int current_time = 0; current_time < end_time; current_time++) {
+        // loop over time intervals
+        int number_arrivals = variable.poisson(arrival_rate);
+
+
+        // current arrival requests  :  优先处理着陆队列
+        for (int i = 0; i < number_arrivals; i++) {
+            Plane current_plane(flight_number++, current_time, arriving);
+            if (land_on.can_land(current_plane) != success)
+            current_plane.refuse( );
+        }
+
+
+        int number_departures = variable.poisson(departure_rate);
+        // current departure requests
+        for (int j = 0; j < number_departures; j++) {
+            Plane current_plane(flight_number++, current_time, departing);
+            if (take_off.can_depart(current_plane) != success)
+            current_plane.refuse( );
+        }
+
+        Plane moving_plane;
+        switch (take_off.activity(current_time, moving_plane)) {
+            // Let at most one Plane onto the Runway at current_time. ??????
+            case land:
+            moving_plane.land(current_time);
+            break;
+            case takeoff:
+            moving_plane.fly(current_time);
+            break;
+            case idle:
+            run_idle_takeoff(current_time);
+        }
+        switch (land_on.activity(current_time, moving_plane)) {
+            // Let at most one Plane onto the Runway at current_time. ??????
+            case land:
+            moving_plane.land(current_time);
+            break;
+            case takeoff:
+            moving_plane.fly(current_time);
+            break;
+            case idle:
+            run_idle_landon(current_time);
+        }
+        // cout << "END LOOPcurrent_time :  " << current_time  <<endl;system("pause");
+    }
+        land_on.shut_down(end_time);
+         take_off.shut_down(end_time);
 }
 
  /*
